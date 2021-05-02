49c9c0ba5710ac6ed6efcf182946afb20b78436b
J0sueTM <teodoro.josue@protonmail.ch>
2021-05-02 03:38:51 +0000

    Typos: Identation

--- inc/video/gl/gl_object.h
+++ inc/video/gl/gl_object.h
@@ -90,10 +90,10 @@ D_vbo_data(struct D_vbo *__vbo,
 void
 D_vao_attrib_pointer(struct D_vao *__vao,
                      struct D_vbo *__vbo,
-                     u32					 __index,
-                     i32					 __size,
-                     u32					 __type,
-                     u32					 __stride,
+                     u32           __index,
+                     i32           __size,
+                     u32           __type,
+                     u32           __stride,
                      size_t        __offset);
 
 
